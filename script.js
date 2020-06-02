48b919fc044bf2af52ed34e635e56d5607e8dbed
Mackenzie Clark <clark.rmackenzie@gmail.com>
2020-06-02 14:22:17 -0400

    RE-DO OF CODE, added ability to call jquery functions

--- script.js
+++ script.js
@@ -1,39 +1,49 @@
-// GIVEN I am taking a code quiz
-    // Declare initial variables
-    var submit = document.getElementById("submit");
-    var score = document.getElementById("score");
-
-// WHEN I click the start button
-submit.addEventListener("click", begin);
-
-function begin() {
-
-}
-
-// THEN a timer starts and I am presented with a question
-var timer = document.getElementById("timer");
-var secondsLeft = 60;
-var initialScore = 0;
-
-timer.textContent = "Time: " + secondsLeft;
-
-// WHEN I answer a question
-// THEN I am presented with another question
-
-
-// WHEN I answer a question incorrectly
-// THEN time is subtracted from the clock
-
-
-// WHEN all questions are answered or the timer reaches 0
-
-
-// THEN the game is over
-
-
-// WHEN the game is over
-// THEN I can save my initials and score
+// RE-DO
 
 
 
-// id beginQuiz, beginQuestions
+
+
+
+
+
+
+// // GIVEN I am taking a code quiz
+//     // Declare initial variables
+//     var submit = document.getElementById("submit");
+//     var score = document.getElementById("score");
+
+// // WHEN I click the start button
+// submit.addEventListener("click", begin);
+
+// function begin() {
+
+// }
+
+// // THEN a timer starts and I am presented with a question
+// var timer = document.getElementById("timer");
+// var secondsLeft = 60;
+// var initialScore = 0;
+
+// timer.textContent = "Time: " + secondsLeft;
+
+// // WHEN I answer a question
+// // THEN I am presented with another question
+
+
+// // WHEN I answer a question incorrectly
+// // THEN time is subtracted from the clock
+
+
+// // WHEN all questions are answered or the timer reaches 0
+
+
+// // THEN the game is over
+
+
+// // WHEN the game is over
+// // THEN I can save my initials and score
+
+
+
+// // id beginQuiz, beginQuestions
